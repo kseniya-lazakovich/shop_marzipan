68dc52c3de54d5a7db043e5516e42ad4eb8e9551
kseniya-lazakovich <kseniyalaz@yandex.ru>
2020-05-01 00:45:40 +0300

    emmited commit
    login view and template

--- Shop/account/urls.py
+++ Shop/account/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import profile, AccountView, LoginFormView, UserRegFormView, PasswordChange
+from .views import profile, user_login
 from django.contrib.auth import views as auth_views
 app_name = 'account'
 
 urlpatterns = [
-    path('', AccountView.as_view(), name='auth'),
-    path('login/', LoginFormView.as_view(), name='login'),
-    path('register/', UserRegFormView.as_view(), name='register'),
+    # path('', AccountView.as_view(), name='auth'),
+    path('login/', user_login, name='login'),
+    # path('register/', UserRegFormView.as_view(), name='register'),
     path('profile/', profile, name='profile'),
-    path('password/', PasswordChange.as_view(), name='password'),
+    # path('password/', PasswordChange.as_view(), name='password'),
     # path('login/', auth_views.LoginView.as_view(), name='login'),    
     path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
     
--- Shop/account/views.py
+++ Shop/account/views.py
@@ -18,72 +18,92 @@ def profile(request):
     """страница профиля"""
     return render(request, 'account/profile.html')
 
+
 class PasswordChange(PasswordChangeView, SuccessMessageMixin, LoginRequiredMixin):
     """ Смена пароля """
     template_name = 'account/password_change.html'
     success_url = reverse_lazy('account:password')
     success_message = 'Пароль пользователя изменен'
 
-    
-class AccountView(TemplateView):
-    """ Обработка get запроса """
-    template_name = 'registration/login.html'
 
-    def get(self, request, *args, **kwargs):
-        login_form = LoginForm(self.request.GET or None)
-        user_form = UserRegForm(self.request.GET or None)
-        context = self.get_context_data(**kwargs)
-        context['login_form'] = login_form
-        context['user_form'] = user_form
-        return self.render_to_response(context)
+# class AccountView(TemplateView):
+#     """ Обработка get запроса """
+#     template_name = 'registration/login.html'
+
+#     def get(self, request, *args, **kwargs):
+#         login_form = LoginForm(self.request.GET or None)
+#         user_form = UserRegForm(self.request.GET or None)
+#         context = self.get_context_data(**kwargs)
+#         context['login_form'] = login_form
+#         context['user_form'] = user_form
+#         return self.render_to_response(context)
 
 
-class LoginFormView(FormView):
-    """ обработка формы входа """
-    form_class = LoginForm
-    template_name = 'account/profile.html'
-    success_url = '/'
+# class LoginFormView(FormView):
+#     """ обработка формы входа """
+#     form_class = LoginForm
+#     template_name = 'account/profile.html'
+#     success_url = '/'
 
-    def post(self, request, *args, **kwargs):
-        login_form = self.form_class(request.POST)
-        user_form = UserRegForm()
-        if login_form.is_valid():
-            cd = login_form.cleaned_data
-            # authenticate() сверяет данные с БД
-            user = authenticate(request, username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                # login() запоминает пользователя в сессии
-                    login(request, user)
-                    return render(request, 'account/profile.html')
-                else:
-                    return render(request, 'registration/logged_out.html')
+#     def post(self, request, *args, **kwargs):
+#         login_form = self.form_class(request.POST)
+#         user_form = UserRegForm()
+#         if login_form.is_valid():
+#             cd = login_form.cleaned_data
+#             # authenticate() сверяет данные с БД
+#             user = authenticate(request, username=cd['username'], password=cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                 # login() запоминает пользователя в сессии
+#                     login(request, user)
+#                     return render(request, 'account/profile.html')
+#                 else:
+#                     return render(request, 'registration/logged_out.html')
+#             else:
+#                 messages.error(request, 'Неправильный логин или пароль')
+#             return self.render_to_response(
+#                 self.get_context_data(
+#                 success=True
+#             )
+#         )
+#         else:
+#             login_form = LoginForm()
+#             return self.render_to_response(self.get_context_data(login_form=login_form, user_form=user_form,))
+
+# class UserRegFormView(FormView):
+#     """ обработка формы регистрации """
+#     form_class = UserRegForm
+#     template_name = 'registration/success.html'
+#     success_url = '/'
+
+#     def post(self, request, *args, **kwargs):
+#         user_form = self.form_class(request.POST)
+#         login_form = LoginForm()
+#         if user_form.is_valid():
+#             new_user = user_form.save(commit=False) #создаем нового польз
+#             new_user.set_password(user_form.cleaned_data['password']) #задаем зашифров пароль
+#             new_user.save() #сохраняем польз
+#             return self.render_to_response(
+#                 self.get_context_data(success=True))
+#         else:
+#             user_form = UserRegForm()
+#             return self.render_to_response(self.get_context_data(user_form=user_form, login_form=login_form))
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request, username=cd['username'], password=cd['password'])
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'account/profile.html')
             else:
-                messages.error(request, 'Неправильный логин или пароль')
-            return self.render_to_response(
-                self.get_context_data(
-                success=True
-            )
-        )
+                return render(request, 'registration/logget_out.html')
         else:
-            login_form = LoginForm()
-            return self.render_to_response(self.get_context_data(login_form=login_form, user_form=user_form,))
-
-class UserRegFormView(FormView):
-    """ обработка формы регистрации """
-    form_class = UserRegForm
-    template_name = 'registration/success.html'
-    success_url = '/'
-
-    def post(self, request, *args, **kwargs):
-        user_form = self.form_class(request.POST)
-        login_form = LoginForm()
-        if user_form.is_valid():
-            new_user = user_form.save(commit=False) #создаем нового польз
-            new_user.set_password(user_form.cleaned_data['password']) #задаем зашифров пароль
-            new_user.save() #сохраняем польз
-            return self.render_to_response(
-                self.get_context_data(success=True))
-        else:
-            user_form = UserRegForm()
-            return self.render_to_response(self.get_context_data(user_form=user_form, login_form=login_form))
+            messages.warning(request, 'Неверный логин или пароль')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
