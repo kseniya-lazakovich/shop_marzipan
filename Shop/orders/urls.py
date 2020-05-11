88ddca805dfb750b1fcb0afd8cc40b1a58af7003
kseniya-lazakovich <kseniyalaz@yandex.ru>
2020-05-11 16:40:59 +0300

    emmited commit
    email order

--- Shop/orders/urls.py
+++ Shop/orders/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import order_created
+from .views import order_create
 
 app_name = 'orders'
 
 urlpatterns = [
-    path('create/', order_created, name='order'),
+    path('create/', order_create, name='order'),
 ]
--- Shop/orders/views.py
+++ Shop/orders/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from .models import OrderItem
+from .models import OrderItem, Order
+from django.core.mail import send_mail
 from .forms import OrderCreateForm
 from cart.cart import Cart
 
 
-def order_created(request):
+def order_created(order_id):
+    """Задача отправки email-уведомлений при успешном оформлении заказа."""
+    order = Order.objects.get(id=order_id)
+    subject = 'Order nr. {}'.format(order.id)
+    message = 'Дорогой {},\n\nВаш заказ оформлен и принят в обработку.\n'\
+              'Ваши товары: {}'\
+              'Номер заказа №{}.'.format(order.first_name, order.id)
+    mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
+    return mail_sent
+
+
+def order_create(request):
     cart = Cart(request)
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
@@ -12,8 +24,9 @@ def order_created(request):
             order = form.save()
             for item in cart:
                 OrderItem.objects.create(order=order, product=item['product'], price=item[
-                                         'price'], quantity=item['quantity'])
+                    'price'], quantity=item['quantity'])
             cart.clear()
+            order_created(order.id)
             return render(request, 'orders/created.html', {'order': order})
     else:
         form = OrderCreateForm()
